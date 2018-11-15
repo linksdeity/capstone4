56f600529c7378fd95baf95a7e46a87e8f282aec
Tim Broughton <TimBroughton@quickenloans.com>
2018-11-15 15:01:46 -0500

    added checkout button

--- javascript.js
+++ javascript.js
@@ -1,11 +1,18 @@
 
-var pizza = {name:"Pizza", price: 1.25};
-var burger = {name:"Burger", price: 1.75};
-var television = {name:"Television", price: 899.99};
-var candy = {name:"Candy", price: 0.75};
+var plaindog = {name:"Plain Dog", price: 1.00};
+var chilidog = {name:"Chili Dog", price: 1.75};
+var deluxechilidog = {name:"Deluxe Chili Dog", price: 2.25};
+var footlong = {name:"Foot Long Chili Dog", price: 3.25};
+var footlongdeluxe = {name:"Foot Long Deluxe Chili Dog", price: 3.75};
+var yardlong = {name:"Yard Long Chili Dog", price: 4.25};
+var yardlongdeluxe = {name:"Yard Long Deluxe Chili Dog", price: 4.75};
+var turbodog = {name:"Turbo Chili Dog", price: 5.00};
+var superdog = {name:"Super Chili Dog", price: 7.00};
+var hyperdog = {name:"Hyper Chili Dog", price: 10.00};
 
-var items = [pizza, burger, television, candy];
+var items = [plaindog, chilidog, deluxechilidog, footlong, footlongdeluxe, yardlong, yardlongdeluxe, turbodog, superdog, hyperdog];
 var cost =[];
+var purchasing = [];
 
 var selectedItem;
 var price = 0.00;
@@ -18,7 +25,8 @@ for (i = 0; i < items.length; i++)
         selectedItem = items[this.id];
     }
     node.id = i;
-    var textnode = document.createTextNode(items[i].name + " - - - price: " + items[i].price);
+    node.className = "product";
+    var textnode = document.createTextNode(items[i].name + " - - - price: $" + items[i].price);
     node.appendChild(textnode);
     document.getElementById("forSale").appendChild(node);
 }
@@ -32,6 +40,7 @@ function addToCart(){
     document.getElementById("cartList").appendChild(node);
 
     cost.push(selectedItem.price);
+    purchasing.push(selectedItem);
 
     totalCost = 0;
 
@@ -39,5 +48,44 @@ function addToCart(){
         totalCost += element;
     });
 
-    document.getElementById("cost").innerHTML = totalCost;
+    document.getElementById("total").innerHTML = totalCost.toFixed(2);
+}
+
+var btnContainer = document.getElementById("forSale");
+
+var btns = btnContainer.getElementsByClassName("product");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("product-active");
+
+    if (current.length > 0) { 
+      current[0].className = current[0].className.replace(" product-active", "");
+    }
+
+    this.className += " product-active";
+  });
+}
+
+var receipt;
+
+function checkOut(){
+
+    receipt = "";
+
+    purchasing.forEach(element => {
+        receipt += element.name + " - - " + element.price.toFixed(2) + "\n";
+    });
+
+    alert("Here is your receipt...\n" + receipt + "\nTotal: " + totalCost.toFixed(2));
+
+    totalCost = 0.00
+    document.getElementById("total").innerHTML = 0.00.toFixed(2);
+    cost =[];
+    purchasing = [];
+
+    while( document.getElementById("cartList").firstChild ){
+        document.getElementById("cartList").removeChild( document.getElementById("cartList").firstChild );
+    }
+
 }
